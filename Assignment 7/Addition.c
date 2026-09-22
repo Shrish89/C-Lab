66f4f15d616ed4643873eaa8112b0d0733e1eab7
Shrish <139778785+Shrish89@users.noreply.github.com>
2026-09-23 03:07:07 +0530

    Add files via upload

---
+++ Assignment 7/Addition.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main()
+{
+    int r1, c1, r2, c2, i, j;
+
+    printf("Enter rows and columns of first matrix: ");
+    scanf("%d %d", &r1, &c1);
+
+    printf("Enter rows and columns of second matrix: ");
+    scanf("%d %d", &r2, &c2);
+
+    if(r1 != r2 || c1 != c2)
+    {
+        printf("Matrices cannot be added.");
+        return 0;
+    }
+
+    int a[r1][c1], b[r2][c2], sum[r1][c1];
+
+    printf("Enter elements of first matrix:\n");
+    for(i = 0; i < r1; i++)
+    {
+        for(j = 0; j < c1; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of second matrix:\n");
+    for(i = 0; i < r2; i++)
+    {
+        for(j = 0; j < c2; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for(i = 0; i < r1; i++)
+    {
+        for(j = 0; j < c1; j++)
+        {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    printf("Sum of matrices:\n");
+    for(i = 0; i < r1; i++)
+    {
+        for(j = 0; j < c1; j++)
+        {
+            printf("%d\t", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
